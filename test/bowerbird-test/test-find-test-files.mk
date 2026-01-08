98ae86e9c5cf8b0d48b3b7ce7ab3712f87b8f16d
Jeff Fredenburg <136350727+jfredenburg@users.noreply.github.com>
2026-01-08 11:59:25 -0600

    Update test-find-test-files counts for new mock files
    Updated expected file counts from 6 to 7 to account for:
    - mock-test-multiline.mk
    - mock-test-prefixed.mk

--- test/bowerbird-test/test-find-test-files.mk
+++ test/bowerbird-test/test-find-test-files.mk
@@ -3,7 +3,7 @@ $(call bowerbird::test::find-test-files,test/mock-tests,mock-test*.mk)
 endef
 
 test-find-test-files-count:
-	$(call bowerbird::test::compare-strings,6,$(words $(bowerbird::test::mock-test-files)))
+	$(call bowerbird::test::compare-strings,7,$(words $(bowerbird::test::mock-test-files)))
 
 test-find-test-files-alpha:
 	$(call bowerbird::test::compare-sets,$(filter %alpha.mk,$(bowerbird::test::mock-test-files)),$(abspath test/mock-tests/alpha/mock-test-alpha.mk))
@@ -33,10 +33,10 @@ test-find-test-files-pattern-alpha:
 	$(call bowerbird::test::compare-strings,1,$(words $(call bowerbird::test::find-test-files,test/mock-tests,*alpha.mk)))
 
 test-find-test-files-pattern-all-mk:
-	$(call bowerbird::test::compare-strings,6,$(words $(call bowerbird::test::find-test-files,test/mock-tests,*.mk)))
+	$(call bowerbird::test::compare-strings,7,$(words $(call bowerbird::test::find-test-files,test/mock-tests,*.mk)))
 
 test-find-test-files-subdir-only:
 	$(call bowerbird::test::compare-strings,3,$(words $(call bowerbird::test::find-test-files,test/mock-tests/alpha,*.mk)))
 
 test-find-test-files-total-count:
-	$(call bowerbird::test::compare-strings,6,$(words $(call bowerbird::test::find-test-files,test/mock-tests,mock-test*.mk)))
+	$(call bowerbird::test::compare-strings,7,$(words $(call bowerbird::test::find-test-files,test/mock-tests,mock-test*.mk)))
